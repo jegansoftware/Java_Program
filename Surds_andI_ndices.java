c21edad1752599664a5fd7d317b6516b75729270
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-21 15:54:39 +0530

    Create  Surds_andI_ndices.java

---
+++ Surds_andI_ndices.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+		double a;
+		double b;
+		a=Math.pow(5,0.25);
+		b=Math.pow(125,0.25);
+		a=a*b;
+		
+		System.out.printf("Answer Is : %.2f",a);
+		
+	}
+}
